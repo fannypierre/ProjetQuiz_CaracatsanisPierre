3deffa19d405e6349dd9a406889ba6092bb63d92
fannypierre <fannypierre@users.noreply.github.com>
2020-03-10 22:20:14 +0100

    Header + Head (à revoir)

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
+
 <?php session_start();?>
 <?php if(isset($_SESSION["login"])) $user = $_SESSION["login"]; else $user = "User";?>
 
 <html lang="fr">
-	<head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	  <link rel="stylesheet" href="css/accueil.css"/>
-	</head>
-
+	
+	<?php require_once "head.php"; ?>
 	<?php require_once "header.php"; ?>
 	<body>
 	    <p>Test</p>
@@ -22,7 +17,7 @@
 		} catch (PDOException $e) {
 		    echo 'Connexion échouée : ' . $e->getMessage();
 		}   
-	     ?>
+	    ?>
 
 	    <div class="contenant">
 	    	<p>Affichage des questions :</p>
---
+++ login.php
@@ -0,0 +1,7 @@
+<?php require_once "head.php"; ?>
+<?php require_once "header.php"; ?>
+<body>
+	<div class="container-fluid">
+	  <img src="images/login.png">
+	</div>
+</body>
