4e4a12812bee5c4145219ce27154135219563841
fannypierre <fannypierre@users.noreply.github.com>
2020-03-11 17:30:11 +0100

    Header finit (normalement)

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-light bg-light row justify-content-center">
-    <div class="row justify-content-center">
-        <div class="col-4">
-          <img src="images/Logo.svg" width="60%" class="d-inline-block align-top" alt="">
+<nav id="navigation-bar" class="navbar navbar-light bg-light row justify-content-center">
+    <div class="row container justify-content-center">
+        <div class="col-4" id="logo-navbar">
+          <img src="images/Logo.svg" width="20%" class="d-inline-block align-top" alt="">
         </div>
         <div class="col-4">
           <h2>QUIZ</h2>
