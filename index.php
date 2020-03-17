9082037c9ae61bc7b13e7d68cfcc4db0deec062d
fannypierre <fannypierre@users.noreply.github.com>
2020-03-17 12:45:32 +0100

    Page d'acceuil (connexion) avec popup pour le login (pas l'inscription). ATTENTION : Pas encore de PHP

--- index.php
+++ index.php
@@ -8,26 +8,34 @@
 	<?php require_once "head.php"; ?>
 	<?php require_once "header.php"; ?>
 	<body>
-	    <p>Test</p>
+	    <div class="container-fluid" id="index">
+	    	<div id="phrase-accroche">
+	    		<h1>Phrase d'accroche</h1>
+	    	</div>
+	    	
+	    	<div id="boutons-connexion">
+	    		<a type="button" class="btn btn-outline-dark btn-lg" href="#login">Se connecter</a>
+				<a type="button" class="btn btn-outline-dark btn-lg">S'inscrire</a>
+	    	</div>
+		</div>
 
-	    <?php
-	    try {
-		    $bdd = new PDO("mysql:host=localhost;dbname=id12709408_quizprojet", "root", "root");
-	        $questions = $bdd -> query("SELECT * FROM QUESTION");
-		} catch (PDOException $e) {
-		    echo 'Connexion échouée : ' . $e->getMessage();
-		}   
-	    ?>
-
-	    <div class="contenant">
-	    	<p>Affichage des questions :</p>
-	        <?php
-	            foreach($questions as $question)
-	            {
-	                echo $question["LibelleQuestion"] . "</br>";
-	            }
-	         ?>
-	    </div>
-
+		<div class="login" id="login">
+		    <div class="popup-inner">
+		    	<img src="images/Logo.svg" width="20%" class="d-inline-block align-top" alt="">
+		        <h2>QUIZ</h2>
+		    	<form id="login-form">
+				  	<div class="form-group">
+				    	<label for="connexion-email" id="connexion-label">Login</label> <!-- !! Récupérer l'email !! -->
+				    	<input type="email" class="form-control" id="connexion-email" placeholder="Ex : jdupond">
+				  	</div>
+				 	<div class="form-group">
+				    	<label for="connexion-mdp" id="connexion-label">Mot de passe</label> <!-- !! Récupérer le MDP !! -->
+				    	<input type="password" class="form-control" id="connexion-mdp">
+				  	</div>
+				  	<button type="submit" class="btn" id="login-bouton-valider">Valider</button>
+				</form>
+				<a class="closepopup" href="">X</a>
+			</div>
+		</div>
 	</body>
 </html>
