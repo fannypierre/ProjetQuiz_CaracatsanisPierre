e1ce1d510d4ef04ca620a8d1790a6bbbbec8e97b
fannypierre <fannypierre@users.noreply.github.com>
2020-03-17 14:33:04 +0100

    HTML/CSS de l'acceuil finis

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	    	
 	    	<div id="boutons-connexion">
 	    		<a type="button" class="btn btn-outline-dark btn-lg" href="#login">Se connecter</a>
-				<a type="button" class="btn btn-outline-dark btn-lg">S'inscrire</a>
+				<a type="button" class="btn btn-outline-dark btn-lg" href="#inscription">S'inscrire</a>
 	    	</div>
 		</div>
 
@@ -25,14 +25,36 @@
 		        <h2>QUIZ</h2>
 		    	<form id="login-form">
 				  	<div class="form-group">
-				    	<label for="connexion-email" id="connexion-label">Login</label> <!-- !! Récupérer l'email !! -->
-				    	<input type="email" class="form-control" id="connexion-email" placeholder="Ex : jdupond">
+				    	<label for="connexion-email" id="connexion-label">Email</label> <!-- !! Récupérer l'email !! -->
+				    	<input type="email" class="form-control" id="connexion-email" placeholder="Ex : jdupond@ensc.fr">
 				  	</div>
 				 	<div class="form-group">
 				    	<label for="connexion-mdp" id="connexion-label">Mot de passe</label> <!-- !! Récupérer le MDP !! -->
 				    	<input type="password" class="form-control" id="connexion-mdp">
 				  	</div>
 				  	<button type="submit" class="btn" id="login-bouton-valider">Valider</button>
+				</form>
+				<a class="closepopup" href="">X</a>
+			</div>
+		</div>
+		<div class="login" id="inscription">
+		    <div class="popup-inner inscription">
+		    	<img src="images/Logo.svg" width="20%" class="d-inline-block align-top" alt="">
+		        <h2>QUIZ</h2>
+		    	<form id="inscription-form">
+				  	<div class="form-group">
+				    	<label for="inscription-email" id="inscription-label">Email</label> <!-- !! Récupérer l'email !! -->
+				    	<input type="email" class="form-control" id="inscription-email" placeholder="Ex : jdupond@ensc.fr">
+				  	</div>
+				 	<div class="form-group">
+				    	<label for="inscription-mdp" id="inscription-label">Mot de passe</label> <!-- !! Récupérer le MDP !! -->
+				    	<input type="password" class="form-control" id="inscription-mdp">
+				  	</div>
+				  	<div class="form-group">
+				    	<label for="inscription-confirmation-mdp" id="inscription-label">Confirmez le mot de passe</label>
+				    	<input type="password" class="form-control" id="inscription-confirmation-mdp">
+				  	</div>
+				  	<button type="submit" class="btn" id="inscription-bouton-valider">Valider</button>
 				</form>
 				<a class="closepopup" href="">X</a>
 			</div>
