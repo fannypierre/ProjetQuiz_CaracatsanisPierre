050ffd4aa84eec06987512c738e7c49734cb70fe
fannypierre <fannypierre@users.noreply.github.com>
2020-03-17 08:58:25 +0100

    Ajout d'un fichier d'exemple de connexion sur la BD ainsi que la syntaxe pour accéder à ces données

---
+++ ExempleConnexionBD.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<?php session_start();?>
+<?php if(isset($_SESSION["login"])) $user = $_SESSION["login"]; else $user = "User";?>
+
+<html lang="fr">
+	
+	<?php require_once "head.php"; ?>
+	<?php require_once "header.php"; ?>
+	<body>
+	    <p>Test</p>
+
+	    <?php
+	    try {
+		    $bdd = new PDO("mysql:host=localhost;dbname=id12709408_quizprojet", "root", "root");
+	        $questions = $bdd -> query("SELECT * FROM QUESTION");
+		} catch (PDOException $e) {
+		    echo 'Connexion échouée : ' . $e->getMessage();
+		}   
+	    ?>
+
+	    <div class="contenant">
+	    	<p>Affichage des questions :</p>
+	        <?php
+	            foreach($questions as $question)
+	            {
+	                echo $question["LibelleQuestion"] . "</br>";
+	            }
+	         ?>
+	    </div>
+
+	</body>
+</html>
