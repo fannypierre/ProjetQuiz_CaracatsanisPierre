e78c2c6ae7407958edc6e8f5b2c45f50f3fa2ba7
fannypierre <fannypierre@users.noreply.github.com>
2020-04-04 11:07:06 +0200

    Barre de navigation avec les liens vers les pages d'accueil et de profil joueur (pas très responsive...)

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,15 @@
 <nav id="navigation-bar" class="navbar navbar-light bg-light row justify-content-center">
+
+	<a href="accueilQuiz.php"><img id="nav-logos" src="images/logo-home.png" width="20%" class="d-inline-block align-top" alt="" ></a>
     <div class="row container justify-content-center">
+
         <div class="col-4" id="logo-navbar">
           <img src="images/Logo.svg" width="20%" class="d-inline-block align-top" alt="">
         </div>
         <div class="col-4" id="header_text">
           <h2 id="header_quizzz">QUIZZZ</h2>
         </div>
+
     </div>
+    <a href="profilJoueur.php"><img id="nav-logos" src="images/logo-profilJoueur.png" width="20%" class="d-inline-block align-top" alt=""></a>
 </nav>
