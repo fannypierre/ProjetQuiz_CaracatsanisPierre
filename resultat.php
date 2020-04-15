9f58fff65938b654814c3131af6e42e387412fe0
fannypierre <fannypierre@users.noreply.github.com>
2020-04-15 15:58:25 +0200

    Ajout vérification login dans Resulat.php et questions.php

--- resultat.php
+++ resultat.php
@@ -1,7 +1,10 @@
 <?php 
 session_start();
-if(isset($_SESSION["login"])) $user = $_SESSION["login"]; 
-else $user = "User";
+if (isset($_SESSION["email"])) {
+    $user = $_SESSION["email"];
+} else {
+    $user = "user";
+}
 
 require_once "includes/head.php";
 require_once "includes/header.php";
